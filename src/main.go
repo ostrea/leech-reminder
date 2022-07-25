0cc634c19fdc163aa67b04a046993b738670d802
ostrea <9917165+ostrea@users.noreply.github.com>
2022-07-25 19:25:40 +0300

    Add main logic

--- src/main.go
+++ src/main.go
@@ -1,10 +1,43 @@
 package main
 
-import "github.com/gen2brain/beeep"
+import (
+	"fmt"
+	"leech-reminder/src/anki"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/gen2brain/beeep"
+)
 
 func main() {
-	err := beeep.Notify("Hello", "World", "")
+	leeches, err := anki.GetLeeches()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	rand.Seed(time.Now().Unix())
+	var usedThisSession = make(map[string]struct{})
+	for {
+		var randomLeech = leeches[rand.Intn(len(leeches))]
+		if _, ok := usedThisSession[randomLeech.Expression]; ok {
+			continue
+		}
+
+		err := beeep.Notify("Leeeeech!", randomLeech.Expression, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(5 * time.Second)
+
+		answer := fmt.Sprintf("%s\n%s", randomLeech.Reading, randomLeech.Definition)
+		err = beeep.Notify("Answer", answer, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		usedThisSession[randomLeech.Expression] = struct{}{}
+		time.Sleep(10 * time.Minute)
 	}
 }
